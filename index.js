e3103c3acfe3003fb4562dabaea41de5958fb143
Sy Le <lenguyensy+mbp@gmail.com>
2017-01-04 20:46:28 -0800

    white space

--- index.js
+++ index.js
@@ -16,64 +16,64 @@ var concat = require('gulp-concat');
 
 //compile styles
 function _compileStyles( sytlePath, destination, bundledName ){
-	return function(){
-		return gulp.src( sytlePath )
-	        .pipe( sass().on('error', sass.logError) )
-	        .pipe( concat(bundledName) )
-	        .pipe( _getPathAsGulpConfig( destination ) );
-    }
+  return function(){
+    return gulp.src( sytlePath )
+      .pipe( sass().on('error', sass.logError) )
+      .pipe( concat(bundledName) )
+      .pipe( _getPathAsGulpConfig( destination ) );
+  }
 }
 
 function _compileJs( jsPath, destination, bundledName, babelifyConfig, aliasifyConfig, ignoreSourceMap ) {
-	return function(){
-		var browserifyJsPipe = browserify( {
-				entries: jsPath,
-				debug: true
-			})
-			.transform(babelify, _.size(babelifyConfig) > 0 ? babelifyConfig : {})
-			.transform(aliasify, _.size(aliasifyConfig) > 0 ? aliasifyConfig : {})//aliasify config
-			.bundle()
-			.pipe( source( bundledName ) );
+  return function(){
+    var browserifyJsPipe = browserify( {
+        entries: jsPath,
+        debug: true
+      })
+      .transform(babelify, _.size(babelifyConfig) > 0 ? babelifyConfig : {})
+      .transform(aliasify, _.size(aliasifyConfig) > 0 ? aliasifyConfig : {})//aliasify config
+      .bundle()
+      .pipe( source( bundledName ) );
 
-		if( ignoreSourceMap !== true){
-			//advance mode with source map
-			browserifyJsPipe = browserifyJsPipe.pipe(buffer())
-			    .pipe(sourcemaps.init({loadMaps: true}))
-			        .pipe(uglify({
-			        	mangle: false
-			        }))
-			        .on('error', gutil.log)
-			    .pipe(sourcemaps.write('./'))
-		}
+    if( ignoreSourceMap !== true){
+      //advance mode with source map
+      browserifyJsPipe = browserifyJsPipe.pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+          .pipe(uglify({
+            mangle: false
+          }))
+          .on('error', gutil.log)
+        .pipe(sourcemaps.write('./'))
+    }
 
-		return browserifyJsPipe.pipe( _getPathAsGulpConfig( destination ) );
-	}
+    return browserifyJsPipe.pipe( _getPathAsGulpConfig( destination ) );
+  }
 }
 
 //copy file from src to destination
 function _copyFile( src, destination ){
-	return function(){
-		return gulp.src( src )
-	    		.pipe( _getPathAsGulpConfig( destination ) );
-	};
+  return function(){
+    return gulp.src( src )
+        .pipe( _getPathAsGulpConfig( destination ) );
+  };
 }
 
 function _concatFiles( src, destination, bundledName ){
-	return function(){
-		return gulp.src( src )
-	        .pipe( concat(bundledName) )
-	        .pipe( _getPathAsGulpConfig( destination ) );
-    }
+  return function(){
+    return gulp.src( src )
+      .pipe( concat(bundledName) )
+      .pipe( _getPathAsGulpConfig( destination ) );
+  }
 }
 
 //get gulp dest regardless if inpgut is string or gulp dest object
 function _getPathAsGulpConfig( path ){
-	return _.isString( path ) ? gulp.dest( path ) : path;
+  return _.isString( path ) ? gulp.dest( path ) : path;
 }
 
 module.exports = {
-	compileStyles : _compileStyles,
-	compileJs : _compileJs,
-	copyFile : _copyFile,
-	concatFiles : _concatFiles
+  compileStyles : _compileStyles,
+  compileJs : _compileJs,
+  copyFile : _copyFile,
+  concatFiles : _concatFiles
 }
